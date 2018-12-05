07c83d60e13c9d24ef5a905f58b0950f3a2d999a
Shreyansh Pandey (c) <shreyanshp@vmware.com>
2018-12-05 15:06:18 +0530

    [Project : MoisesMacero] : Skeleton Project Created

---
+++ MoisesMacero/src/main/java/com/seventhHacker/MoisesMacero/MoisesMaceroApplication.java
@@ -0,0 +1,12 @@
+package com.seventhHacker.MoisesMacero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoisesMaceroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoisesMaceroApplication.class, args);
+	}
+}
---
+++ MoisesMacero/src/test/java/com/seventhHacker/MoisesMacero/MoisesMaceroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seventhHacker.MoisesMacero;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoisesMaceroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
